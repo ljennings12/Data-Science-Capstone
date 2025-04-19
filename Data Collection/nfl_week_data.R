0f5e60eb7c3a01c78954da769d5590cdff304c1d
Liam Jennings <liam.j.jennings12@gmail.com>
2025-04-19 12:02:30 -0400

    Added defensive metrics that were missing in combined dataset

--- Data Collection/nfl_week_data.R
+++ Data Collection/nfl_week_data.R
@@ -92,7 +92,7 @@ defensive_metrics_reshape <- {
       away_team = team_away_team,
       week = week_home_team,
       opposing_pass_rate_away_team:epa_allowed_home_team,
-      avg_drive_time_sec_allowed_away_team:avg_time_of_possession_allowed_home_team
+      avg_total_yards_allowed_away_team:avg_time_of_possession_allowed_home_team
     )
 }
 
@@ -111,3 +111,30 @@ nfl_team_week_data <- {
     # join independent factors
     inner_join(independent_metrics)
 }
+
+
+
+# Created Combined Variables ----------------------------------------------
+
+# nfl_team_combined_data <- {
+#   nfl_team_week_data |> 
+#     mutate(
+#       combined_avg_offensive_yards_per_game = avg_total_yards_away_team + avg_total_yards_home_team,
+#       
+#       combined_avg_ppg = ppg_away_team + ppg_home_team,
+#       
+#       combined_avg_possessions_per_game = possessions_per_game_away_team + possessions_per_game_home_team,
+#       
+#       combined_avg_giveaways_per_game = giveaways_per_game_away_team + giveaways_per_game_home_team,
+#       
+#       combined_avg_points_per_poss = points_per_poss_away_team + points_per_poss_home_team,
+#       
+#       combined_avg_pass_rate = (pass_rate_away_team + pass_rate_home_team) / 2,
+#       
+#       combined_avg_rush_rate = (rush_rate_away_team + rush_rate_home_team) / 2,
+#       
+#       combined_avg_proe = (proe_away_team + proe_home_team) / 2,
+#       
+#       combined_avg_epa = epa_away_team + epa_home_team
+#     )
+# }
